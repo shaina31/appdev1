cbc86cdbea18b503e56e41ad33660e8149b9f209
shaina31 <shaina.pagarigan31@gmail.com>
2024-10-01 20:38:37 +0800

    I’m done completing the section on creating and nesting co mponents.

---
+++ react-quickstart/src/App.jsx
@@ -0,0 +1,18 @@
+function MyButton() {
+  return (
+    <button>I'm a Button!</button>
+  );
+}
+
+export function MyApp() {
+  return (
+    <>
+      <div>
+      <h1>Welcome to My App</h1>
+      <MyButton /> 
+      </div>
+    </>
+  );
+}
+
+export default MyApp;
