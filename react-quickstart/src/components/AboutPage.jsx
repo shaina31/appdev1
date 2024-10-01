1092a3acd2bb3ad62ac23053026b540a8d94eceb
shaina31 <shaina.pagarigan31@gmail.com>
2024-10-02 00:41:48 +0800

    I'm done completing the section on Adding styles.

--- react-quickstart/src/components/AboutPage.jsx
+++ react-quickstart/src/components/AboutPage.jsx
@@ -5,6 +5,7 @@ function AboutPage() {
         <div>
             <h1>About</h1>
             <p>Hello there. <br /> How do you do?</p>
+            <img className="avatar" />
         </div>
     );
 }
